8754b48ecf56680ada0d6902ceea98b6b1d97507
Jörg Dölfer <doelfer@gmail.com>
2023-05-02 13:53:21 +0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Codewars Java
+
+Katas from <https://www.codewars.com/dashboard>[Codewars Dashboard]
---
+++ src/main/java/com/joedoe/cwjava/StringEndsWithKata.java
@@ -0,0 +1,9 @@
+package com.joedoe.cwjava;
+
+class StringEndsWithKata {
+
+    public static boolean solution(String str, String ending) {
+        return false;
+    }
+
+}
