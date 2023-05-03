8c529873b019b8186f8f9bbba544ef47938445ad
Jörg Dölfer <doelfer@gmail.com>
2023-05-03 10:45:54 +0200

    binary tree

---
+++ src/main/java/com/joedoe/cwjava/BinaryTreeWithIncorrectValue.java
@@ -0,0 +1,14 @@
+package com.joedoe.cwjava;
+
+public class BinaryTreeWithIncorrectValue {
+
+    /**
+     * Finding the incorrect value in a Binary Tree
+     * 6 kyu
+     * https://www.codewars.com/kata/63f13a354a828b0041979359/train/java
+     */    
+    public static int[] findIncorrectNumber(int[] tree) {
+        return new int[] {0,0};
+      }
+
+}
---
+++ src/test/java/com/joedoe/cwjava/BinaryTreeWithIncorrectValueTest.java
@@ -0,0 +1,45 @@
+package com.joedoe.cwjava;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class BinaryTreeWithIncorrectValueTest {
+
+  @Test
+  public void testTreeWith3Levels() {
+    
+      final int[][] expected = {{0,27}, {1,13}, {2,14}, {6,7}};
+    
+      final int[][] input = new int[4][];
+      input[0] = new int[] {28, 13, 14, 6, 7, 5, 9};
+      input[1] = new int[] {27, 14, 14, 6, 7, 5, 9};
+      input[2] = new int[] {27, 13, 15, 6, 7, 5, 9};
+      input[3] = new int[] {29, 13, 16, 5, 8, 9, 1};
+    
+      final int[][] actual = new int[4][];
+      for (int test = 0; test < input.length; test++) {
+        actual[test] = BinaryTreeWithIncorrectValue.findIncorrectNumber(input[test]);
+        assertArrayEquals("not as expected for 3 level test " + test, expected[test], actual[test]);
+      } 
+  }
+
+  @Test
+  public void testTreeWith4Levels() {
+    
+      final int[][] expected = {{0,19}, {2,10}, {3,4}, {8,1}};
+    
+      final int[][] input = new int[4][];
+      input[0] = new int[] {21, 9, 10, 4, 5, 4, 6, 2, 2, 1, 4, 1, 3, 2, 4};
+      input[1] = new int[] {19, 9, 12, 4, 5, 4, 6, 2, 2, 1, 4, 1, 3, 2, 4};
+      input[2] = new int[] {19, 9, 10, 5, 5, 4, 6, 2, 2, 1, 4, 1, 3, 2, 4};
+      input[3] = new int[] {19, 9, 10, 4, 5, 4, 6, 3, 2, 1, 4, 1, 3, 2, 4};
+    
+      final int[][] actual = new int[4][];
+      for (int test = 0; test < input.length; test++) {
+        actual[test] = BinaryTreeWithIncorrectValue.findIncorrectNumber(input[test]);
+        assertArrayEquals("not as expected for 4 level test " + test, expected[test], actual[test]);
+      }  
+  }
+
+}
