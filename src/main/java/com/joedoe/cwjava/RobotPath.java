6ae7b75feede725806518f02a4b809cf37431b9d
joedoe <doelfer@googlemail.com>
2023-05-07 17:35:51 +0200

    robot (initial)

---
+++ src/main/java/com/joedoe/cwjava/RobotPath.java
@@ -0,0 +1,14 @@
+package com.joedoe.cwjava;
+
+public class RobotPath {
+
+    /**
+     * You are a Robot: Translating a Path
+     * 6 kyu
+     * https://www.codewars.com/kata/636173d79cf0de003d6834e4/train/java
+     */
+    public static String walk(String path) {
+        return "Paused";
+    }
+
+}
---
+++ src/test/java/com/joedoe/cwjava/RobotPathTest.java
@@ -0,0 +1,15 @@
+package com.joedoe.cwjava;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  @Test
+  void testSomething() {
+    assertEquals("Take 1 step up", RobotPath.walk("^"));
+    assertEquals("Take 1 step down", RobotPath.walk("v"));
+    assertEquals("Take 2 steps right", RobotPath.walk(">>"));
+    assertEquals("Take 2 steps left\nTake 1 step right", RobotPath.walk("<<>"));
+    assertEquals("Paused", RobotPath.walk(""));
+  }
+}
