f459b6eac160fadecc6a6f3e06c2dd7bea74f40e
Jörg Dölfer <doelfer@gmail.com>
2023-05-09 10:23:21 +0200

    bud (initial)

---
+++ src/main/java/com/joedoe/cwjava/Bud.java
@@ -0,0 +1,14 @@
+package com.joedoe.cwjava;
+
+public class Bud {
+
+    /**
+     * Buddy Pairs
+     * 5 kyu
+     * https://www.codewars.com/kata/59ccf051dcc4050f7800008f/train/java
+     */
+    public static String buddy(long start, long limit) {
+        return "";
+    }
+    
+}
---
+++ src/test/java/com/joedoe/cwjava/BudTest.java
@@ -0,0 +1,22 @@
+package com.joedoe.cwjava;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BudTest {
+
+    private static void testing(long start, long limit, String expected) {
+        System.out.println("start: " + start);
+        System.out.println("limit: " + limit);
+        String actual = Bud.buddy(start, limit);
+        System.out.println("Expect: " + expected);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test() {
+        testing(1071625, 1103735, "(1081184 1331967)");
+        testing(2382, 3679, "Nothing");
+        testing(381, 4318, "(1050 1925)");
+    }
+}
