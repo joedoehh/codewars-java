c8da2beb53681ba4c4c80cf173bd462fa479c164
Jörg Dölfer <doelfer@gmail.com>
2023-05-09 15:38:17 +0200

    bud (intermediate

--- src/main/java/com/joedoe/cwjava/Bud.java
+++ src/main/java/com/joedoe/cwjava/Bud.java
@@ -1,5 +1,8 @@
 package com.joedoe.cwjava;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Bud {
 
     /**
@@ -8,7 +11,37 @@ public class Bud {
      * https://www.codewars.com/kata/59ccf051dcc4050f7800008f/train/java
      */
     public static String buddy(long start, long limit) {
-        return "";
+        for (long i = start; i <= limit; i++) {
+            Set<Long> divisors = properDivisors(i);
+            long buddy = sum(divisors)-1;
+            if (isBuddyOf(i, buddy))
+                return String.format("(%d %d)", i, buddy);
+        }
+        return "Nothing";
     }
-    
+
+    private static boolean isBuddyOf(long m, long n) {
+        return  sum(properDivisors(n))-1 == m;
+    }
+
+    private static long sum(Set<Long> divisors) {
+        long result = 0;
+        for (Long x : divisors) 
+            result += x;
+        return result;
+    }
+
+    private static Set<Long> properDivisors(long n) {
+        Set<Long> result = new HashSet<>();
+        if (n != 1)
+            result.add(1L);
+        for (long i = 2; i <= Math.sqrt(n); i++) {
+            if ((n % i) == 0)
+                result.add(i);
+            if ((n % (n/i)) == 0)
+                result.add(n / i);
+        }
+        return result;
+    }
+
 }
--- src/test/java/com/joedoe/cwjava/BudTest.java
+++ src/test/java/com/joedoe/cwjava/BudTest.java
@@ -15,8 +15,9 @@ public class BudTest {
     }
     @Test
     public void test() {
-        testing(1071625, 1103735, "(1081184 1331967)");
-        testing(2382, 3679, "Nothing");
+        testing(48, 100, "(48 75)");
         testing(381, 4318, "(1050 1925)");
+        testing(2382, 3679, "Nothing");
+        testing(1071625, 1103735, "(1081184 1331967)");
     }
 }
