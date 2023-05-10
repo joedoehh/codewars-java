eebb7f186f8c26f383183662b31e58afc5dcc2ec
Jörg Dölfer <doelfer@gmail.com>
2023-05-10 10:54:37 +0200

    human readbale time init

---
+++ src/main/java/com/joedoe/cwjava/HumanReadableTime.java
@@ -0,0 +1,16 @@
+package com.joedoe.cwjava;
+
+public class HumanReadableTime {
+
+    /**
+     * Human readable times
+     * 5 kyu
+     * https://www.codewars.com/kata/52685f7382004e774f0001f7/train/java
+     */
+    public static String makeReadable(int seconds) {
+        // Do something
+        return Integer.toString(seconds);
+      }
+
+
+}
