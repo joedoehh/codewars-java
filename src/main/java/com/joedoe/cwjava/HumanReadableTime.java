e2189582616f33a3faf31a4cd30b7191121fb6b9
Jörg Dölfer <doelfer@gmail.com>
2023-05-10 11:17:29 +0200

    human readable solved

--- src/main/java/com/joedoe/cwjava/HumanReadableTime.java
+++ src/main/java/com/joedoe/cwjava/HumanReadableTime.java
@@ -8,9 +8,15 @@ public class HumanReadableTime {
      * https://www.codewars.com/kata/52685f7382004e774f0001f7/train/java
      */
     public static String makeReadable(int seconds) {
-        // Do something
-        return Integer.toString(seconds);
-      }
+        int s = seconds % 60;
+        int h = seconds / 60;
+        int m = h % 60;
+        h = h / 60;
+        return String.format("%s:%s:%s", toPaddedRepresentation(h), toPaddedRepresentation(m), toPaddedRepresentation(s));
+    }
 
+    private static String toPaddedRepresentation(int nr) {
+        return (nr < 10) ? "0" + nr : "" + nr;
+    }
 
 }
